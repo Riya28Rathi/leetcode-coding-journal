fb47c604a542a17fbfc439dfcd93e5026e343b22
Riya Rathi <158172048+Riya28Rathi@users.noreply.github.com>
2024-10-08 18:52:57 +0530

    Time: 27 ms (5.17%), Space: 48.1 MB (6.44%) - LeetHub

---
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.java
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        if(root == null){
+            return -1;
+        }
+        
+        int max=Integer.MIN_VALUE;
+        int maxKey=0;
+        // HashMap<Integer, Integer> maxMap=new HashMap<>();
+        int count=1;
+        // Stack<TreeNode> parent=new LinkedList<>();
+        // Stack<TreeNode> child=new LinkedList<>();
+
+        LinkedList<TreeNode> parent = new LinkedList<>();
+        LinkedList<TreeNode> child = new LinkedList<>();
+
+        parent.push(root);
+
+        while(!parent.isEmpty() || !child.isEmpty()){
+
+            if(parent.isEmpty()){
+                parent.addAll(child); // Copy all elements
+
+                // Clear the original stack
+                child.clear();
+
+            } 
+
+            int temp=0;
+            while(!parent.isEmpty()){
+
+                TreeNode node=parent.pop();
+                temp+=node.val;
+                if(node.left != null){
+                    child.push(node.left);
+                }
+                if(node.right != null){
+                    child.push(node.right);
+                }
+                
+
+            }
+            // max=Math.max(max, temp);
+            System.out.println("max:  "+ max);
+            System.out.println("temp:  "+ temp);
+            if(temp> max){
+                max=temp;
+                maxKey=count;
+            }
+            // maxMap.put(count,temp);
+            count++;
+
+        }
+
+
+
+        return maxKey;
+
+        
+    }
+}
