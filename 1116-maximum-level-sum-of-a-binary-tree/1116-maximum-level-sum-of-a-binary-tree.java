85197496b80110af23c9f195e24f11dd5de4c758
Riya Rathi <158172048+Riya28Rathi@users.noreply.github.com>
2024-10-08 19:03:16 +0530

    Time: 27 ms (5.17%), Space: 47.9 MB (6.44%) - LeetHub

--- 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.java
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.java
@@ -14,60 +14,123 @@
  * }
  */
 class Solution {
+
+    // =============method1=======================================
+    // public int maxLevelSum(TreeNode root) {
+    //     if(root == null){
+    //         return -1;
+    //     }
+        
+    //     int max=Integer.MIN_VALUE;
+    //     int maxKey=0;
+    //     // HashMap<Integer, Integer> maxMap=new HashMap<>();
+    //     int count=1;
+        
+    //     Stack<TreeNode> parent = new Stack<>();
+    //     Stack<TreeNode> child = new Stack<>();
+
+    //     parent.push(root);
+
+    //     while(!parent.isEmpty() || !child.isEmpty()){
+
+    //         if(parent.isEmpty()){
+    //             parent.addAll(child); // Copy all elements
+
+    //             // Clear the original stack
+    //             child.clear();
+
+    //         } 
+
+    //         int temp=0;
+    //         while(!parent.isEmpty()){
+
+    //             TreeNode node=parent.pop();
+    //             temp+=node.val;
+    //             if(node.left != null){
+    //                 child.push(node.left);
+    //             }
+    //             if(node.right != null){
+    //                 child.push(node.right);
+    //             }
+                
+
+    //         }
+    //         // max=Math.max(max, temp);
+    //         System.out.println("max:  "+ max);
+    //         System.out.println("temp:  "+ temp);
+    //         if(temp> max){
+    //             max=temp;
+    //             maxKey=count;
+    //         }
+    //         // maxMap.put(count,temp);
+    //         count++;
+
+    //     }
+
+
+
+    //     return maxKey;
+
+        
+    // }
+
+
     public int maxLevelSum(TreeNode root) {
         if(root == null){
             return -1;
         }
         
-        int max=Integer.MIN_VALUE;
-        int maxKey=0;
-        // HashMap<Integer, Integer> maxMap=new HashMap<>();
-        int count=1;
+
+            int max=Integer.MIN_VALUE;
+            int maxKey=0;
+            // HashMap<Integer, Integer> maxMap=new HashMap<>();
+            int count=1;
+            
+            Queue<TreeNode> parent = new LinkedList();
+            Queue<TreeNode> child = new LinkedList();
+
+            parent.add(root);
+
+            while(!parent.isEmpty() || !child.isEmpty()){
+
+                if(parent.isEmpty()){
+                    parent.addAll(child); // Copy all elements
+
+                    // Clear the original stack
+                    child.clear();
+
+                } 
+
+                int temp=0;
+                while(!parent.isEmpty()){
+
+                    TreeNode node=parent.poll();
+                    temp+=node.val;
+                    if(node.left != null){
+                        child.add(node.left);
+                    }
+                    if(node.right != null){
+                        child.add(node.right);
+                    }
+                    
+
+                }
+                // max=Math.max(max, temp);
+                System.out.println("max:  "+ max);
+                System.out.println("temp:  "+ temp);
+                if(temp> max){
+                    max=temp;
+                    maxKey=count;
+                }
+                // maxMap.put(count,temp);
+                count++;
+
+            }
+
+
+
+            return maxKey;
         
-        Stack<TreeNode> parent = new Stack<>();
-        Stack<TreeNode> child = new Stack<>();
-
-        parent.push(root);
-
-        while(!parent.isEmpty() || !child.isEmpty()){
-
-            if(parent.isEmpty()){
-                parent.addAll(child); // Copy all elements
-
-                // Clear the original stack
-                child.clear();
-
-            } 
-
-            int temp=0;
-            while(!parent.isEmpty()){
-
-                TreeNode node=parent.pop();
-                temp+=node.val;
-                if(node.left != null){
-                    child.push(node.left);
-                }
-                if(node.right != null){
-                    child.push(node.right);
-                }
-                
-
-            }
-            // max=Math.max(max, temp);
-            System.out.println("max:  "+ max);
-            System.out.println("temp:  "+ temp);
-            if(temp> max){
-                max=temp;
-                maxKey=count;
-            }
-            // maxMap.put(count,temp);
-            count++;
-
-        }
-
-
-
-        return maxKey;
 
         
     }
